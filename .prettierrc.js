71f2c6c277835585aaf2be2c787cc45bbb15dad4
Burhan YILMAZ <burhan.yilmaaz@gmail.com>
2024-01-07 11:55:27 +0300

    feat: added elint, prettier and husky

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+ module.exports = {
+  arrowParens: 'avoid',
+  bracketSameLine: true,
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+  bracketSpacing: true,
+  printWidth: 1200,
+}
---
+++ src/screens/Test/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from 'react-native';
+
+const TestScreen = () => (
+  <View>
+    <Text>TestScreen</Text>
+  </View>
+);
+
+export default TestScreen;
